55c4339f95d8ad9dcc88cfeda1f9487eba1bb857
oheong <enffl9568@naver.com>
2021-06-11 13:01:08 +0900

    디비 연동, 조회 성공

--- ETL/connect.py
+++ ETL/connect.py
@@ -1,1 +1,25 @@
-dd
+import psycopg2
+
+try:
+    print("*************Connected*************")
+    # DB 연동
+    db = psycopg2.connect(host='localhost',
+                        dbname='ABC-test',
+                        user='postgres',
+                        password='oheong',
+                        port=5432)
+
+    # 명령 처리할때 사용
+    cursor = db.cursor()
+
+    cursor.execute("select * from member order by no;")
+    rows=cursor.fetchall()
+
+    for i in rows:
+        print(i)
+
+    # print(execute())
+
+except psycopg2.DatabaseError as db_err:
+    print("==========Not Connected!==========")
+    print(db_err)
