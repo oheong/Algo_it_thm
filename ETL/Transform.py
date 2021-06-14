7c630a622d91d39374e432aaaf6a4312cb161245
oheong <enffl9568@naver.com>
2021-06-14 11:14:39 +0900

    csv 파일 생성까지 완료

--- ETL/Transform.py
+++ ETL/Transform.py
@@ -1,28 +1,22 @@
-import Extract
+import pandas as pd
+import Connect
+"""
+DB to CSV
+"""
 
-# 두 자리 이하의 숫자로 구성된 리스트를 받아서
-# 1자리일 경우 앞에 0을 붙여 반환
-def num_list_to_double_digit(list):
-    print("=============중간 변환 시작=============")
-    temp_list = [str(data) for data in list]
+list = Connect.rows
 
-    for i, data in enumerate(list):
-        temp_list[i] = data if len(data) == 2 else '0' + data
+for i in list:
+    print(i)
+print("=====================================")
+print(str(len(list))+"개의 데이터 조회 완료")
+print("=====================================")
 
-    print("=============중간 변화 완료=============")
-    return temp_list
+col_name=['이름', '메일', '비밀번호', '생년월일']
+
+df = pd.DataFrame(list, columns=col_name)
+
+df.to_csv('result.csv', index = False, encoding="utf-8-sig")
+print("&&&&&&&&csv 생성 완료&&&&&&&&")
 
 
-hour = num_list_to_double_digit(Extract.hour)
-min = num_list_to_double_digit(Extract.min)
-
-# 데이터 변환 중간 확인
-# print(hour)
-# print(min)
-
-#데이터 변환
-hm = [hour[i] + ":" + min[i] for i in range(len(hour))]
-
-print(str(len(hm))+"개의 데이터 변환 완료")
-#데이터 변환 확인
-# print(hm)
--- ETL/connect.py
+++ ETL/connect.py
@@ -1,5 +1,11 @@
 import psycopg2
 
+"""
+1. 대상 DB접속
+2. postgresql 원천 데이터 조회 
+3. postgresql 원천 데이터 추출
+"""
+
 try:
     print("*************Connected*************")
     # DB 연동
@@ -14,19 +20,24 @@ try:
 
     # 실행 할 쿼리문
     sql_job : str = rf'''
-        select * 
+        select 
+               name,
+               mail,
+               password,
+               to_char(birth,'YYYY-MM-DD')
         from member
         where 1=1
         order by no;
     '''
 
+    # Extract
     # 쿼리 실행
     cursor.execute(sql_job)
     rows=cursor.fetchall()
 
     # 출력 (잘 나옴)
-    for i in rows:
-        print(i)
+    # for i in rows:
+    #     print(i)
 
 except psycopg2.DatabaseError as db_err:
     print("==========Not Connected!==========")
